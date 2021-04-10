17f7b0e935f7da3456e2726d3e11241d361ad4f7
Victor Emanuel <victor.emanuel@esig.com.br>
2021-04-10 17:54:14 -0300

    adds: set priority, set cpu, filter task and process tree

--- gerenciador-tarefas/gerenciador_processos.cpp
+++ gerenciador-tarefas/gerenciador_processos.cpp
@@ -36,7 +36,6 @@ void killTask()
     int pid = promptInt("PID");
     printf("Process %d was ", pid);
     cout << (kill(pid, SIGKILL) ? "NOT killed due to an error" : "successfully killed");
-
     cout << endl;
 }
 
@@ -61,22 +60,24 @@ void continueTask()
 void setTaskPriority()
 {
     cout << "SET PROCESS PRIORITY\n";
-    int pid = promptInt("PID"), priority = promptInt("Priority");
-    printf("Process %d is ", pid);
+    int pid = promptInt("PID"), priority = promptInt("Nice");
+    printf("Process %d nice was ", pid);
+    cout << (setpriority(PRIO_PROCESS, pid, priority) ? "NOT changed due to an error" : "successfully changed");
+    cout << endl;
 }
 
 void setTaskCore()
 {
     cout << "SET PROCESS CPU\n";
     int pid = promptInt("PID"), core = promptInt("New core");
-
     cpu_set_t mask;
     CPU_ZERO(&mask);
     CPU_SET(core, &mask);
 
-    printf("Process %d is ", pid);
-    cout << sched_setaffinity(pid, sizeof(mask), &mask) ? "NOT" : "";
+    printf("Process %d was ", pid);
+    cout << (sched_setaffinity(pid, sizeof(mask), &mask) ? "NOT changed due to an error" : "successfully changed");
     printf(" running on core %d", core);
+
     // if (sched_setaffinity(pid, sizeof(mask), &mask) == 0)
     // {
     //     printf("now running on core %d", core);
@@ -91,12 +92,23 @@ void setTaskCore()
 
 void filterTask()
 {
+    string nome;
     cout << "FILTER PROCESS\n";
-    int pid = promptInt("PID");
+    cout << "Enter process name: ";
+    cin >> nome;
+    cout << "Result: " << endl;
+    system((view_processes_command + " | grep " + nome).c_str());
+    system("read -p \"Enter to continue\" Exiting");
 }
 
 void taskTree()
 {
+    char treeCommand[40];
+    cout << "PROCESS TREE\n";
+    int pid = promptInt("PID");
+    sprintf(treeCommand, "pstree %d -u", pid);
+    system((treeCommand));
+    system("read -p \"Enter to continue\" Exiting");
 }
 
 int main(int argc, char const *argv[])
@@ -114,7 +126,7 @@ int main(int argc, char const *argv[])
         cout << "\n5 - Set core";
         cout << "\n6 - Task Tree";
         cout << "\n7 - Filter";
-        cout << "\n8 - Close\n\n";
+        cout << "\n9 - Close\n\n";
 
         switch (option)
         {
@@ -139,11 +151,11 @@ int main(int argc, char const *argv[])
             break;
 
         case 6:
-            filterTask();
+            taskTree();
             break;
 
         case 7:
-            taskTree();
+            filterTask();
             break;
 
         case 0:
@@ -151,7 +163,7 @@ int main(int argc, char const *argv[])
             break;
         }
 
-        cout << "\n\nChoose command to run: ";
+        cout << "\nChoose command to run: ";
     } while (cin >> option && option != 8);
 
     cout << "Program finnished\n";
