c8dfaf8b77ad26dbd4cf9d25cd2e1f562892ec2f
Victor Emanuel <victor.emanuel@esig.com.br>
2021-04-24 01:25:57 -0300

    starts last work, 'trens_sincronizados'

---
+++ trens_sincronizados/src/sample/Main.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private Parent createContent() {
+        return new StackPane(new Text("Hello World"));
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        stage.setScene(new Scene(createContent(), 300, 300));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
